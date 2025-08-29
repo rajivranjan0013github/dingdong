00ddd5fadf78d5d94bde52344e42225aa15ff49a
rajiv <rajivranjan0013@gmail.com>
2025-08-29 11:59:09 +0530

    v-4

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,3 @@
-// export const API_URL = 'https://app.thebilling.in';
-export const API_URL = 'http://10.249.211.152:3002';
+export const API_URL = 'https://app.thebilling.in';
+// export const API_URL = 'http://10.249.211.152:3002';
 
